4e0a7c041b4fee465bad030134e766d37e9b070a
Angela Lengkeek <angelalengkeek@gmail.com>
2016-11-12 13:23:41 +0100

    Added more image resources

--- src/main/java/com/push/MyPushFilter.java
+++ src/main/java/com/push/MyPushFilter.java
@@ -2,10 +2,7 @@
 package com.push;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-
-import com.push.*;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -13,6 +10,8 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.eclipse.jetty.server.PushBuilder;
 import org.eclipse.jetty.server.Request;
 
@@ -21,36 +20,42 @@ public final class MyPushFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 
-		System.out.println("Do Filter wordt geroepen");
+		HttpServletResponse res = (HttpServletResponse) response;
+
+		// optional no caching options for use with automated browser testing
+		// res.setHeader("Cache-Control", "no-cache, no-store,must-revalidate");
+		// res.setHeader("Pragma", "no-cache");
+		// res.setHeader("Expires", "0");
+
 		// if http/1.1 then return. HTTP/2 is running only on secure ports.
 		if (request.isSecure() == false) {
-			chain.doFilter(request, response);
+			chain.doFilter(request, res);
 			return;
 		}
+
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		// System.out.println("HttpRequest is" + httpRequest );
 		String uri = httpRequest.getRequestURI();
 		String fileName = uri.substring(uri.lastIndexOf('/') + 1, uri.length());
-		System.out.println("Switch uri is " + fileName);
+
 		// implement a basic push implementation here
 		switch (fileName) {
 		case "index.html":
+
 			List<String> resourcesToPush = ResourceUtil.getHtmlImgReferences(fileName);
 			PushBuilder pushBuilder = Request.getBaseRequest(request).getPushBuilder();
-			
+
 			for (String resource : resourcesToPush) {
 				pushBuilder.path(resource).push();
+
 			}
-			
 			break;
 		default:
 			break;
 
 		}
-		
-		
+
 		// pass the request along the filter chain
-		chain.doFilter(request, response);
+		chain.doFilter(request, res);
 	}
 
 	public void destroy() {
--- src/main/java/com/push/ResourceUtil.java
+++ src/main/java/com/push/ResourceUtil.java
@@ -1,8 +1,6 @@
 package com.push;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,19 +15,25 @@ import com.google.common.io.Files;
 
 public class ResourceUtil {
 
-	
+	// TODO this singleton needs to be enhanced (CDI) - for now suffices
+	// to make sure static algoritm is executed one time to prevent overhead
+
+	private static List<String> results = new ArrayList<String>();
+
 	public static List<String> getHtmlImgReferences(String fileName) throws IOException {
 
-		// put requested webapp file in a string so that Jsoup can parse it
-		String content = Files.toString(new File("webapps/standalonepush/"+fileName), Charsets.UTF_8);
+		if (results.isEmpty()) {
 
-		Document doc = Jsoup.parseBodyFragment(content);
-		Elements media = doc.select("img[src]");
-		
-		//Add all the elements in a list so we can return this to te 
-		List<String> results = new ArrayList<String>();
-		for (Element src : media) {
-			results.add(src.attr("src"));
+			// put requested webapp file in a string so that Jsoup can parse it
+			String content = Files.toString(new File("webapps/standalonepush/" + fileName), Charsets.UTF_8);
+
+			Document doc = Jsoup.parseBodyFragment(content);
+			Elements media = doc.select("img[src]");
+
+			// Add all the elements in a list so we can return this
+			for (Element src : media) {
+				results.add(src.attr("src"));
+			}
 		}
 		return results;
 
